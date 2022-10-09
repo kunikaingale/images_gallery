8b2620936e28abeb9d2f66341a92459b19fe5714
kunika <kunika.ingale@gmail.com>
2022-10-09 13:04:07 +0530

    first API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,22 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
-
+const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY
 const App=()=> {
   const [word,setWord]=useState('');
   const handleSearchSubmit=(event)=>{
     event.preventDefault()
     console.log(word)
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((result)=>result.json())
+      .then((data)=>{
+        console.log(data)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
   }
-  console.log(word)
+  
   return (
     <div>
       <Header title="Images Gallery"/>
