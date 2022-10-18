2a869435766930a3b42fcf90d6267f0d1b4e73c2
kunika <kunika.ingale@gmail.com>
2022-10-18 12:55:17 +0530

    deleting prettier settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,35 @@
 //import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import { useState } from 'react';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
 
-const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY
-const App=()=> {
-  const [word,setWord]=useState('');
-  const handleSearchSubmit=(event)=>{
-    event.preventDefault()
-    console.log(word)
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-      .then((result)=>result.json())
-      .then((data)=>{
-        console.log(data)
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const App = () => {
+  const [word, setWord] = useState("");
+  const [images,setImages]=useState([])
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
       })
-      .catch((error)=>{
-        console.log(error)
-      })
-  
-  }
-  
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search image={handleSearchSubmit} word={word} setWord={setWord}/>
+      <Header title="Images Gallery" />
+      <Search image={handleSearchSubmit} word={word} setWord={setWord} />
     </div>
   );
-}
+};
 
 export default App;
