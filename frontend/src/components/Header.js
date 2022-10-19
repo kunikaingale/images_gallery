d7bd1830913933eb7410a1fb99943e996d29c98a
kunika <kunika.ingale@gmail.com>
2022-10-19 21:57:01 +0530

    added text logo header section

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
+import logo from '../images/logo.png'
 
 const navbarStyle = { backgroundColor: "lightblue" };
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <img src={logo} alt="logo"></img>
       </Container>
     </Navbar>
   );
